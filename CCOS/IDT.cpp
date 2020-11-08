16784f179f09da24ee34a5d63e5e2e19eaa1ef4a
godofthunder8756 <64430471+godofthunder8756@users.noreply.github.com>
2020-11-08 17:56:59 -0500

    Fixed Some Keyboard Interrupt Issues

--- CCOS/IDT.cpp
+++ CCOS/IDT.cpp
@@ -15,15 +15,16 @@ extern uint_64 isr1;
 extern "C" void LoadIDT();
 
 void InitializeIDT(){
-	for (uint_64 t = 0; t < 256; t++) {
-		_idt[t].zero = 0;
-		_idt[t].offset_low = (uint_16)(((uint_64)&isr1 & 0x000000000000ffff));
-		_idt[t].offset_mid = (uint_16)(((uint_64)&isr1 & 0x000000000ffff0000) >> 16);
-		_idt[t].offset_high = (uint_64)(((uint_64)&isr1 & 0xffffffff00000000) >> 32);
-		_idt[t].ist = 0;
-		_idt[t].selector = 0x08;
-		_idt[t].types_attr = 0x8e;
-	}
+
+_idt[1].zero = 0;
+_idt[1].offset_low = (uint_16)(((uint_64)&isr1 & 0x000000000000ffff));
+_idt[1].offset_mid = (uint_16)(((uint_64)&isr1 & 0x000000000ffff0000) >> 16);
+_idt[1].offset_high = (uint_64)(((uint_64)&isr1 & 0xffffffff88888888) >> 32);
+_idt[1].ist = 0;
+_idt[1].selector = 0x08;
+_idt[1].types_attr = 0x8e;
+
+RemapPic();
 
 	outb(0x21, 0xfd);
 	outb(0xa1, 0xff);
