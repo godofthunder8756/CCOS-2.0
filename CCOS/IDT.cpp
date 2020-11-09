e1c93d5c6d903eab67403cbef95b8116192d9eb8
godofthunder8756 <64430471+godofthunder8756@users.noreply.github.com>
2020-11-08 23:46:14 -0500

    Added Backspace Support

--- CCOS/IDT.cpp
+++ CCOS/IDT.cpp
@@ -36,7 +36,7 @@ void (*MainKeyBoardHandler)(uint_8 scanCode, uint_8 chr);
 
 extern "C" void isr1_handler() {
 	uint_8 scanCode = inb(0x60);
-	uint_8 chr;
+	uint_8 chr=0;
 
 	if (scanCode < 0x3A) {
 		chr = KBSet1::ScanCodeLookupTable[scanCode];
--- CCOS/kernel.cpp
+++ CCOS/kernel.cpp
@@ -1,7 +1,20 @@
 #include "TextPrint.cpp"
 #include "IDT.cpp"
 
-void // Part 14 1:08
+void KeyboardHandler(uint_8 scanCode, uint_8 chr) {
+	if (chr != 0) {
+		PrintChar(chr);
+	}
+	else {
+		switch (scanCode) {
+		case 0x8E: //Backspace
+				SetCursorPosition(CursorPosition - 1);
+				PrintChar(' ');
+				SetCursorPosition(CursorPosition - 1);
+				break;
+		}
+	}
+}
 
 extern const char Logo[];
 extern const char Info[];
@@ -10,7 +23,7 @@ extern const char Info[];
 extern "C" void _start() {
 	SetCursorPosition(PositionFromCoords(0, 0));
 	InitializeIDT();
-
+	MainKeyBoardHandler = KeyboardHandler;
 	PrintString(Logo);
 	PrintString("\n\r");
 	PrintString(Info);
